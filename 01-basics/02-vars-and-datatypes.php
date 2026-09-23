f01e5f27960229e4096c9e3be65ea765f24fb7c7
Ferdy <ferdy.fiers@gmail.com>
2026-09-23 10:39:09 +0200

    Starter files

---
+++ 01-basics/02-vars-and-datatypes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+
+<body>
+    
+	<?php 
+        //========== Variabelen
+
+
+
+        //========== Array uitlezen
+
+
+
+
+	?>
+
+
+
+    <a href="_index.php" class='previousTopic'>Ga naar vorig topic</a>
+    <a href="02TASK-secret-agent.php" class='nextTopic'>Ga naar volgend topic</a>
+    <script src="../app.js"></script>
+</body>
+
+</html>
